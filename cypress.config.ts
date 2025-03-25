7173b3014c907b14f4675a83b5e8403828450689
Dominica Wannenburg <dominicawan@gmail.com>
2025-03-25 14:03:25 +0200

    Install and setup Cypress + Cucumber

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,40 @@
 import { defineConfig } from 'cypress'
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild'
+import 'dotenv/config'
+
+console.log('createBundler:', createBundler)
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions,
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config)
+
+  on(
+    'file:preprocessor',
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    }),
+  )
+
+  return config
+}
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
+    baseUrl: process.env.CYPRESS_BASE_URL,
+    specPattern: 'cypress/integration/**/*.{feature,features}',
+    supportFile: 'cypress/support/index.{js,jsx,ts,tsx}',
+    setupNodeEvents,
   },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  viewportHeight: 768,
+  viewportWidth: 1024,
+  defaultCommandTimeout: 6000,
+  video: false,
 })
